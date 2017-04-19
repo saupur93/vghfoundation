f87777f477f9584b1aa9b97f544ffb0aacd94aef
ian knauer <ian.knauer@gmail.com>
2017-04-19 10:13:00 -0700

    added background color to narrow-basic-layout for events

--- web/app/themes/vghubc/templates/layouts/layout-narrow_basic_content_area.php
+++ web/app/themes/vghubc/templates/layouts/layout-narrow_basic_content_area.php
@@ -1,7 +1,8 @@
+<?php $event = get_post_type(get_the_ID()) == 'signature_events' ? true : false; ?>
 <?php if($count == 1): ?>
-<section class="panel extra-padded overview-panel narrow-basic-content"<?php echo ' panel-'.$count; ?>>
+<section class="panel extra-padded overview-panel narrow-basic-content <?php echo ' panel-'.$count; ?><?php if($event) print ' category-bg-color'; ?>">
 <?php else: ?>
-<section class="panel padded-top narrow-basic-content<?php echo ' panel-'.$count; ?>">
+<section class="panel padded-top narrow-basic-content <?php echo ' panel-'.$count; ?><?php if($event) print ' category-bg-color'; ?>">
 <?php endif; ?>
   <div class="container">
   	<div class="narrow-wrap">
