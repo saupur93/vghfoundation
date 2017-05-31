704c7fc226c1f68c17f019287480006b9851b192
Peter Szymczak <peter@signals.ca>
2017-05-31 08:54:37 -0700

    getting started on new surgery template

---
+++ web/app/themes/vghubc/single-editorial_themes_post.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+ * Template Name: Simplified Template (2017)
+ * Template Post Type: themes_post
+ */
+
+?>
+<?php get_header(); ?>
+
+	<div class="page-wrap">
+    <?php //get_template_part('templates/layouts/layouts'); ?>
+
+
+
+<section class="panel panel-4 extra-padded cta-panel bg-color-cta">
+    <div class="container">
+      <div class="inner-wrap">
+        <h2>Give Today. Donate to our Surgery Funds.</h2>
+        <p><a class="button white-keyline" href="https://secure.vghfoundation.ca/site/Donation2?df_id=1740&amp;mfc_pref=T&amp;1740.donation=form1">Donate</a></p>
+        <p>For more information, please contact <a href="tel:6048754676">604 875 4676</a></p>
+      </div>
+    </div>
+  </section>
+
+
+
+  </div>
+
+  <?php edit_post_link('edit', '<div class="admin-edit-link">', '</div>'); ?>
+
+<?php get_footer(); ?>
