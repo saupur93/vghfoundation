c0f947d7f34231c82657e29de155e9dc362b27fb
Coleen Wen <coleen@signals.ca>
2017-05-31 13:59:42 -0700

    new theme template and post template

--- web/app/themes/vghubc/single-editorial_themes_post.php
+++ web/app/themes/vghubc/single-editorial_themes_post.php
@@ -7,25 +7,74 @@
 ?>
 <?php get_header(); ?>
 
-	<div class="page-wrap">
-    <?php //get_template_part('templates/layouts/layouts'); ?>
+<div class="page-wrap">
 
+	<div class="container">
+		<div class="inner-wrap">
+			<div class="theme-head surgery">
+				<div class="title">vital</div>
+				<div class="tagline">Future of Surgery</div>
+				<h1>Our goal is to create a single, <br />state-of-the-art surgical program</h1>
+			</div>
+		</div>
+	</div>
 
+	<section class="panel">
+		<div class="container">
+			<div class="inner-wrap">
+				<div class="theme-video-box">
+					<iframe width="100%" height="720" src="https://www.youtube.com/embed/Uz2C7hCGD-U?controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="theme-stories">
+					<a href="" class="story" style="background-image: url('http://placehold.it/500x400');">
+						Darla's Story
+					</a>
+					<a href="" class="story" style="background-image: url('http://placehold.it/500x400');">
+						Dr Umedaly's Story
+					</a>
+					<a href="" class="story" style="background-image: url('http://placehold.it/500x400');">
+						Willie's Story
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
 
-<section class="panel panel-4 extra-padded cta-panel bg-color-cta">
-    <div class="container">
-      <div class="inner-wrap">
-        <h2>Give Today. Donate to our Surgery Funds.</h2>
-        <p><a class="button white-keyline" href="https://secure.vghfoundation.ca/site/Donation2?df_id=1740&amp;mfc_pref=T&amp;1740.donation=form1">Donate</a></p>
-        <p>For more information, please contact <a href="tel:6048754676">604 875 4676</a></p>
-      </div>
-    </div>
-  </section>
+	<section class="panel extra-padded">
+		<div class="container">
+			<div class="narrow-wrap">
+				<p>
+					Together, Vancouver General Hospital (VGH) and UBC Hospital are the cornerstones of surgical excellence in this province. Whether you are dealing with tumors, traumas, or transplants, we're often the only place that can help. The Future of Surgery strengthens the resources of VGH and UBC Hospital as a single, state-of-the-art, efficient, and effective surgical program.
+				</p>
+				<p>
+					Led by this accomlished group of community, business and health care leaders, the Future of Surgery Campagin Cabinet and our donors are contributing to the transformation of health care.
+				</p>
+				<ul>
+					<li>Hybrid operating room at VGH</li>
+					<li>16 new, optimally sized oeprating rooms and a 40-bed peri-operative care unit at VGH</li>
+					<li>Eight bed high acuity unit at UBC Hospital</li>
+					<li>Enhanced inpatient units at UBC Hospital</li>
+				</ul>
+				<div class="theme-learn-more">
+					Learn More about <em>the</em> Future of Surgery
+					<a href="" target="_blank">Download <em>The Case for Support</em></a>
+				</div>
+			</div>
+		</div>
+	</section>
 
+	<section class="panel extra-padded cta-panel bg-color-cta">
+		<div class="container">
+			<div class="inner-wrap">
+				<h2>Give Today. Donate to our Surgery Funds.</h2>
+				<p><a class="button white-keyline" href="https://secure.vghfoundation.ca/site/Donation2?df_id=1740&amp;mfc_pref=T&amp;1740.donation=form1">Donate</a></p>
+				<p>For more information, please contact <a href="tel:6048754676">604 875 4676</a></p>
+			</div>
+		</div>
+	</section>
 
+</div>
 
-  </div>
-
-  <?php edit_post_link('edit', '<div class="admin-edit-link">', '</div>'); ?>
+	<?php edit_post_link('edit', '<div class="admin-edit-link">', '</div>'); ?>
 
 <?php get_footer(); ?>
---
+++ web/app/themes/vghubc/single-editorial_themes_story.php
@@ -0,0 +1,167 @@
+<?php 
+/*
+ * Template Name: Theme Story
+ * Template Post Type: post
+ */
+
+?>
+<?php get_header(); ?>
+
+<div class="theme-story-head" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/ar-bg-v-white.svg'), url('<?php echo get_template_directory_uri(); ?>/assets/img/home-hero-bg.jpg');">
+	<div class="layer-1">
+		<div class="container">
+			<div class="inner-wrap">
+				<div class="title">vital</div>
+				<div class="tagline">Future of Surgery</div>
+			</div>
+		</div>
+	</div>
+	<div class="layer-2">
+		<div class="container">
+			<div class="inner-wrap">
+				<h1>Willie's Story</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="panel theme-story-row text-right">
+	<div class="img-block"></div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<h3>Life-saving Technology Buys Crucial Time for Father of Three</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate molestiae error qui nobis expedita labore impedit! Rem ipsum molestiae voluptatem tempore eaque. Nemo ex officiis dolorem facilis dicta! Sequi, aliquid soluta animi recusandae esse maiores dicta, ea aspernatur omnis accusantium quia veritatis consectetur temporibus veniam assumenda architecto perspiciatis minus autem possimus eum fuga beatae.
+				</p>
+				<p>
+					Obcaecati cum esse, aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row text-right">
+	<div class="img-block">
+		<img src="http://placehold.it/500x400" alt="">
+	</div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<h3>"And then things got worse."</h3>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row text-right">
+	<div class="img-block"></div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<p>
+					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row theme-full-width-img">
+	<div class="img-block">
+		<img src="http://placehold.it/2000x1000" alt="">
+	</div>
+</section>
+
+<section class="panel theme-story-row text-left">
+	<div class="img-block"></div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<p>
+					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row text-left">
+	<div class="img-block">
+		<img src="http://placehold.it/500x400" alt="">
+	</div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<h3>"I became the first person in BC to be kept alive through the use of an ECMO machine."</h3>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row text-left">
+	<div class="img-block"></div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<p>
+					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row text-right">
+	<div class="img-block">
+		<img src="http://placehold.it/500x400" alt="">
+	</div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<h3>"Have I created enough memories in this lifetime for my family? And my answer was 'no'."</h3>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row text-right">
+	<div class="img-block"></div>
+	<div class="text-block">
+		<div class="container">
+			<div class="inner-wrap">
+				<p>
+					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="panel theme-story-row theme-full-width-img last">
+	<div class="img-block">
+		<img src="http://placehold.it/2000x1000" alt="">
+	</div>
+</section>
+
+<section class="panel extra-padded theme-sharing-panel">
+	<div class="container">
+		<div class="inner-wrap">
+			<div class="text-block">
+				<h2>Lorem ipsum dolar sit amet</h2>
+				<p>
+					Join us in supporting the Future of Surgery and improving outcomes for patients like Willie.
+				</p>
+				<a class="share-btn" href="">Join us on Facebook</a>
+			</div>
+			<div class="img-block">
+				<img src="http://placehold.it/350x350" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php edit_post_link('edit', '<div class="admin-edit-link">', '</div>'); ?>
+
+<?php get_footer(); ?>
