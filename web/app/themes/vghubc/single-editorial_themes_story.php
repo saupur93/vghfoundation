7b8897818415d4d7d70b7b1501567a9dc8e687c7
Coleen Wen <coleen@signals.ca>
2017-05-31 15:14:35 -0700

    text content for willie story

--- web/app/themes/vghubc/single-editorial_themes_story.php
+++ web/app/themes/vghubc/single-editorial_themes_story.php
@@ -32,10 +32,7 @@
 			<div class="inner-wrap">
 				<h3>Life-saving Technology Buys Crucial Time for Father of Three</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate molestiae error qui nobis expedita labore impedit! Rem ipsum molestiae voluptatem tempore eaque. Nemo ex officiis dolorem facilis dicta! Sequi, aliquid soluta animi recusandae esse maiores dicta, ea aspernatur omnis accusantium quia veritatis consectetur temporibus veniam assumenda architecto perspiciatis minus autem possimus eum fuga beatae.
-				</p>
-				<p>
-					Obcaecati cum esse, aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+					Last spring, I was kept alive by VGH.
 				</p>
 			</div>
 		</div>
@@ -61,7 +58,7 @@
 		<div class="container">
 			<div class="inner-wrap">
 				<p>
-					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+					Two years ago, I found myself having a dry cough followed by difficulty breathing and shortness of breath. A visit to a respirologist resulted in being diagnosed with Idiopathic Pulmonary Fibrosis (IPF), a disease that causes scarring of the lungs. I was in my early 40s, and was given the devastating news that there was no known cure for IPF and the possibility of only having 2 - 5 years to live.
 				</p>
 			</div>
 		</div>
@@ -80,7 +77,7 @@
 		<div class="container">
 			<div class="inner-wrap">
 				<p>
-					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+					While waiting for a lung transplant donor, my condition deteriorated rapidly and I found myself at VGH with respiratory failure. The physicians made a life-altering decision. I became the first person in BC to be kept alive through the use of an ECMO machine, a piece of equipment that acts as a portable heart lung machine. The machine would keep me alive and conscious while I waited for a lung transplant donor. While barely conscious in the ICU, I recalled asking myself, "Have I created enough memories in this lifetime for my family? And my answer was 'no'."
 				</p>
 			</div>
 		</div>
@@ -106,7 +103,7 @@
 		<div class="container">
 			<div class="inner-wrap">
 				<p>
-					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+					In June, a transplant donor was found. My double lung transplant surgery took about seven hours and I recovered well enough to be discharged a month later, thanks in part to the tremendous care team at VGH that totaled almost 60 people!
 				</p>
 			</div>
 		</div>
@@ -132,7 +129,7 @@
 		<div class="container">
 			<div class="inner-wrap">
 				<p>
-					Aliquam accusantium voluptate incidunt nostrum odio quisquam laboriosam vel reprehenderit dolorum itaque alias at nulla, officiis totam excepturi adipisci nisi voluptatem recusandae, quasi provident explicabo debitis! Omnis excepturi, nostrum. Animi sequi vero eligendi voluptatibus, veniam debitis ipsa illum expedita architecto repellat vitae numquam.
+					The Future of Surgery campaign provides the essential technology and tools needed to safely perform lung transplants like the one that saved me. I am so grateful to VGH & UBC Hospital Foundation's commitment to transforming health care for patients like me. The team at VGH, including Dr. John Yee, Dr. Hussein Kanji and Dr. Gordon Finlayson made it possible for me to have a second lease on life. Without them, I wouldn't be here today celebrating every day with my wife and family.
 				</p>
 			</div>
 		</div>
