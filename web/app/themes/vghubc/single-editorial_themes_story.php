0f4bdc677351bd8bf10e99e1644bd133830069aa
Saurav Purkayastha <sauravpurkayastha@macbook-pro-2.vch.ca>
2017-11-24 11:07:12 -0800

    changes to the single video page

--- web/app/themes/vghubc/single-editorial_themes_story.php
+++ web/app/themes/vghubc/single-editorial_themes_story.php
@@ -32,14 +32,14 @@ $pagecolor = get_field('theme_color', $relatedthemeID);
 				Your browser does not support the video tag.
 			</video>
 		<?php endif; ?>
-		<?php if(is_single(25510)): ?>
+		<?php if((is_single(25510)) ||(is_single(25606))): ?>
 			<video width="100%" height="100%" autoplay loop>
 				<source src="<?php echo get_template_directory_uri(); ?>/assets/img/angel/videobg.mp4" type="video/mp4">
 				Your browser does not support the video tag.
 			</video>
 		<?php endif; ?>
 	</div>
-	<?php if(is_single(25510)): ?>
+	<?php if((is_single(25510)) ||(is_single(25606))): ?>
 	<div class="layer-1">
 		<div class="container">
 			<div class="inner-wrap">
