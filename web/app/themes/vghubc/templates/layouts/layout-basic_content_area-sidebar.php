4a4ed85dda9027d80d0ebd7d930148ebc97c19df
Saurav Purkayastha <sauravpurkayastha@macbook-pro-2.vch.ca>
2017-12-11 14:56:04 -0800

    landing page using anchor tags for 7eleven

--- web/app/themes/vghubc/templates/layouts/layout-basic_content_area-sidebar.php
+++ web/app/themes/vghubc/templates/layouts/layout-basic_content_area-sidebar.php
@@ -1,6 +1,8 @@
 <?php $event = get_post_type(get_the_ID()) == 'signature_events' ? true : false; ?>
 <?php $section_title = null !== get_sub_field('section_title') ? get_sub_field('section_title') : false; ?>
-<?php if(($count == 1) && (! is_single(25347))):  ?>
+<?php
+ $id = get_the_ID();
+if(($count == 1) && ($id != '25347'))):  ?>
   <section class="panel extra-padded overview-panel sidebar-right<?php echo ' panel-'.$count; ?><?php if($event) print ' category-bg-color'; ?>">
 <?php else: ?>
 <section class="panel padded sidebar-right<?php echo ' panel-'.$count; ?><?php if($event) print ' category-bg-color'; ?>">
