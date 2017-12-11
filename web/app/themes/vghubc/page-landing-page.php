5d7abf3818e7f1b44057f65bffe11aad9e4a4f4f
Saurav Purkayastha <sauravpurkayastha@macbook-pro-2.vch.ca>
2017-12-11 15:30:59 -0800

    landing page using anchor tags for 7eleven

--- web/app/themes/vghubc/page-landing-page.php
+++ web/app/themes/vghubc/page-landing-page.php
@@ -317,8 +317,8 @@ Template Name: LandingPage
             </article>
           </div>
         </div>
-        <p id="7-eleven"></p>
       </div>
+      <p id="7-eleven"></p>
     </section>
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
